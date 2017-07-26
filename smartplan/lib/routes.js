5e84e3ade2b35aa019be3c22ea793690014bc5a3
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-07-26 11:15:44 +0200

    Start of the application

---
+++ smartplan/lib/routes.js
@@ -0,0 +1,14 @@
+FlowRouter.route('/', {
+	name : 'home',
+	action() {
+		console.log('test');
+		BlazeLayout.render('HomeLayout');
+	}
+});
+
+FlowRouter.route('/dataLayer', {
+	name : 'dataLayer',
+	action() {
+		BlazeLayout.render('MainLayout', {main: 'dataLayer'});
+	}
+});
