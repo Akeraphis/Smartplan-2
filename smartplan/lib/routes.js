376d40bf5657636d85b7c21a3f95f9e72cddd044
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-07-27 14:48:48 +0200

    Data_Administration

--- smartplan/lib/routes.js
+++ smartplan/lib/routes.js
@@ -1,7 +1,6 @@
 FlowRouter.route('/', {
 	name : 'home',
 	action() {
-		console.log('test');
 		BlazeLayout.render('HomeLayout');
 	}
 });
@@ -10,5 +9,26 @@ FlowRouter.route('/dataLayer', {
 	name : 'dataLayer',
 	action() {
 		BlazeLayout.render('MainLayout', {main: 'DataLayer_Main'});
+	}
+});
+
+FlowRouter.route('/applications', {
+	name : 'applications',
+	action(){
+		BlazeLayout.render('MainLayout', {main: 'Applications_List'});
+	}
+});
+
+FlowRouter.route('/market', {
+	name : 'market',
+	action(){
+		BlazeLayout.render('MainLayout', {main: 'Market_Main'});
+	}
+});
+
+FlowRouter.route('/administration', {
+	name : 'administration',
+	action(){
+		BlazeLayout.render('Administration_Main');
 	}
 });
