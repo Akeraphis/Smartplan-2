3dbee2f6468b685190969fc088a84a6d80e8ca0f
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-08-02 14:58:51 +0200

    Add input field when clicking on the matrix

--- smartplan/client/layouts/Data_Layer/datatable_edit.js
+++ smartplan/client/layouts/Data_Layer/datatable_edit.js
@@ -3,5 +3,22 @@ Template.DataTable_Edit.helpers({
 		var id = FlowRouter.getParam('id');
 		return DataTables.findOne({_id : id});
 	},
+	'settings' : function() {
+		return  {
+			showFilter: true,
+			showNavigation:'auto',
+			rowsPerPage: 50,
+		};
+	},
+});
 
-})
+Template.DataTable_Edit.events({
+	'click .reactive-table tbody tr': function (event) {
+		// set the blog post we'll display details and news for
+		var post = this;
+		Session.set('post', post);
+		console.log(this, event.target);
+		var temp = event.target.innerHTML;
+		event.target.innerHTML = '<input type ="text" value='+temp+'>'
+	},
+});
