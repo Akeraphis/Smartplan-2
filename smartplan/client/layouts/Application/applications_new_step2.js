b169ca138255e0e04abe79d627ee19dbbc894387
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-08-04 11:57:37 +0200

    Modal to create new attribute

--- smartplan/client/layouts/Application/applications_new_step2.js
+++ smartplan/client/layouts/Application/applications_new_step2.js
@@ -4,3 +4,12 @@ Template.Applications_New_Step2.helpers({
 		return Applications.findOne({_id : app_id});
 	}
 });
+
+Template.Applications_New_Step2.events({
+	'click .new-att': function(){
+		console.log('New attribute');
+		var app_id = FlowRouter.getParam('id');
+		var name="test";
+		Meteor.call("create_attribute", app_id, name);
+	}
+})
--- smartplan/server/Applications/applications.js
+++ smartplan/server/Applications/applications.js
@@ -1,5 +1,8 @@
 Meteor.methods({
 	'create_New_App' : function(name){
-		return Applications.insert({name : name, author : this.userId, createdAt : new Date()});
+		return Applications.insert({name : name, author : this.userId, createdAt : new Date(), attributes : []});
+	},
+	'create_attribute' : function(app_id, name){
+		Applications.update({_id : app_id}, {$push : {attributes : {name : name}}});
 	}
 })
