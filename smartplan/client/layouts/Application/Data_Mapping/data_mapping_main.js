f0cbf3c37a7b53e020aa42b86375e8fe037ee297
Akeraphis <jeanbaptiste.clouard@gmail.com>
2017-08-10 15:52:01 +0200

    data_mapping table

---
+++ smartplan/client/layouts/Application/Data_Mapping/data_mapping_main.js
@@ -0,0 +1,13 @@
+Template.Applications_DataMapping.onCreated(function(){
+	var self = this;
+	self.autorun(function(){
+		var app_id = FlowRouter.getParam('id');
+		self.subscribe("attributes_for_app", app_id);
+	})
+});
+
+Template.Applications_DataMapping.helpers({
+	'getApp' : function(){
+		return Applications.findOne({_id : FlowRouter.getParam('id')});
+	}
+});
